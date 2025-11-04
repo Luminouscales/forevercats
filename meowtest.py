32d741a93fa4fe1cfcaea5e671c508755105bfc0
Luminouscales <loremdrache@gmail.com>
2025-11-04 14:00:33 +0100

    Update meowtest.py

--- meowtest.py
+++ meowtest.py
@@ -8,7 +8,12 @@ debug = True # Shows background stuff like flag changes and locked dialogue flag
 # All flags, globally.
 tbl_flags = {
     'meow': False, # Flag test. Meow meow meow!
-    'unique': False # Told Debug you want to be unique
+    'unique': False, # Told Debug you want to be unique
+    'lr_sawjoint': False, # Saw the joint in the bowl already
+    'lr_havejoint': False, # Took the joint from the bowl
+    'lr_smokedjoint': False, # Smoked the joint
+    'lr_havelighter': False, # Picked up your lighter from the kitchen
+    'lr_snortedshit': False, # Slammed the unidentified powder from underneath the table
 }
 
 # Skillpoints
@@ -93,6 +98,7 @@ def PrintNested( text ):
             # It's returned by DoSkillCheck(). temp is False when it should skip automatically
             temp = True
 
+            # If a value in a dialogue sequence isn't a string it means it's a skillcheck function and we run DoSkillCheck()
             if not isinstance( row[0], str ):
                 temp = row[0]()
             else:
@@ -117,11 +123,11 @@ def DoConditional( text, flag ):
 # They're imperfect just like you and will either aid or disadvantage you
 # You can choose one which will always talk, the others have only a chance
 tbl_skillpoints = {
-    'Electrochemistry': True,
-    'Ars feline': True,
-    'Encyclopedia': True,
-    'Debugging': True,
-    'Serendipity': True
+    'Electrochemistry': True, # Drugs, decadence and the pleasure of having a body. Force ecstasy into your flesh
+    'Ars feline': True, # Connection with the kitty realm, also clever thinking and avoiding principles. Pet cats and lie to people
+    'Encyclopedia': True, # General knowledge about the items of the world and their histories. Give trivia and be curious
+    'Debugging': True, # The ability to approach and solve problems with a cool head, also your bond with technology. Think logically and understand crash logs
+    'Serendipity': True # Dreams, premonitions, psychedelia, the fear of obliteration. Talk to your heart and believe what you can't see
 }
 
 # Text is the text to be printed and skill is the associated skill, checking if it's active. If not, there's only a chance it will speak.
@@ -250,6 +256,8 @@ def seq_cat_leave():
             skip()
             PrintNested( "..........." )
             PrintNested( "Brown and yellow splotches" )
+            intro = False
+            goto_LivingRoom()
         case 2:
             PrintNested("Slowly drifting down. The cat is watching you. It feels like a smirk.")
 
@@ -270,9 +278,6 @@ def gotoDebugIntro():
             case 6: seq_cat_leave()
             case _: skip()
 
-gotoDebugIntro()
-
-
 # LIVING ROOM
 
 # Living room. You were staring at the cupboard
@@ -282,23 +287,143 @@ gotoDebugIntro()
 # You bring it back and you talk
 # You can also watch the skyline a few times to unlock a locked dialogue option
 
+# living room text hub goes here
 
-def gotoLivingRoom():
+# Living room skill dialogue
+DSC_sdangry = partial( DoSkillCheck, 
+    "[SERENDIPITY] Somewhere, a few metres above your horns, someone sips a sweet, lukewarm tea in the kitchen, unable to sleep. Squinting, they are planning out the words of reprimand.", 
+    "Serendipity"
+)
+DSC_sdshots = partial( DoSkillCheck, 
+    "[SERENDIPITY] A couple are still full. Someone filled them, convinced their body could stomach another shot. That time never came.",
+    "Serendipity"
+)
+DSC_ecdump = partial( DoSkillCheck, 
+    "[ELECTROCHEMISTRY] This is the natural environment of the afterparty. I can't blame anyone - teeth-clenching peaks of euphoria aren't a time to be neat and tidy. You know that. The sight is homely to you.",
+    "Electrochemistry"
+)
+DSC_ectemp = partial( DoSkillCheck, 
+    "[] ",
+    ""
+)
+DSC_ecbowl = partial( DoSkillCheck, 
+    "[ELECTROCHEMISTRY] Everyone knows the morning cigarette is the most important meal of the day.",
+    "Electrochemistry"
+)
+
+lr_seqceiling = "Gaze at the ceiling."
+lr_seqceiling_r = [["Nothing notable. The honey lamp hurts your furtive, soft gaze."], ["Little grey clouds of spider webs hang loosely from the corners of the room, long desolate."],
+    ["The upstairs neighbours are quiet. It is the middle of the night, after all. The crickets outside agree."],
+    [DSC_sdangry]]
+
+lr_seqtrash = "Try to concern yourself with the trash around the room."
+lr_seqtrash_r = [["It's dire. The carpet is spikey with crushed fragments of chips."],
+    ["The table cloth is begging for a distant laundry day. Prominent stains of various alcohol drinks paint lakes and oceans, like on a geographical map."],
+    ["There's a red cereal bowl filled with ash and stubs, surrounded by its many shot glass children."],
+    [DSC_sdshots],
+    ["A bustling metropolis of glassy vodka skyscrapers is situated near the sofa, shaping and curving the lights of the room into a quiet night life."],
+    ["Overall, it looks like shit."],
+    [DSC_ecdump],
+    ["It'll take a while to make neat again, even for a sober person, and neither you or Mefedron will be sober for a good while. She can use the help nonetheless."]
+    ]
+
+# Inputs for looking at the clutter
+# Bowl
+# Look under the table
+# Go back
+
+lr_seqtrash_bowl = "Curiously peep into the cereal bowl."
+lr_seqtrash_bowl_r = [["An inconspicuous bowl."], 
+    ["Okay, no, it looks really weird and grotesque. What was once a morning treat now an ashtray for habitual smokers, dried out bottles of alcohol orbiting it."],
+    [DSC_ecbowl],
+    ["A nasty little image of filling the bowl with milk intrudes on your mind and it makes you shiver."],]
+
+# Here you have to check if you have seen the joint and whether you have taken it
+
+lr_eventjoint_first = [[]]
+
+def lr_eventjoint():
+    # If seeing it for the first time
+    if not ReturnFlag( "lr_sawjoint" ):
+        # [ELECTROCHEMISTRY] Wait. Wait, look.
+            # What?
+        # [ELECTROCHEMISTRY] A white dove sits buried with the cigarette butts, forgotten and abandoned.
+            # Oh, it's a joint.
+        # [ELECTROCHEMISTRY] Yes, and not just any joint. It's been barely touched. Okay, like more than half of it is burnt.
+        # [ELECTROCHEMISTRY] But with your weak head, it might just be enough to get you a little woozy.
+        # [ELECTROCHEMISTRY] Well, you going for it?
+            # I don't see a lighter anywhere...
+        # [ELECTROCHEMISTRY] Ah, fuck. Of course. Someone probably took your lighter, and the rest are buried in the clutter.
+        # [ELECTROCHEMISTRY] Just look around and come back later. There must be at least one fire source in her nest of decadence.
+        # [DEBUGGING] Actually, did you not leave your lighter in the kitchen? You absent-mindedly emptied your pockets a few hours ago.
+        # [SERENDIPITY] Someone runs their paw pad against the flint wheel. The air remains cold.
+
+        print("")
+    # If you have seen it already and it's still there
+    elif not ReturnFlag( "lr_tookjoint"):
+        # The joint is happy to see you return, resting in the ash. You can feel the positive energy emanating from it.
+        # [ARS FELINE] We will tell you, there's a cat out there who could really use some soft haziness in their grey, floppy ears.
+        # [ELECTROCHEMISTRY] No, no, kittens. WE would really like to smoke, too.
+        # ACT: Smoke the join right here and now
+        #
+        # ACT: Take the joint without smoking it (you won't be able to smoke it later)
+        # Despite the noisy cravings in your mind, you neatly pocket the joint so as not to spill it.
+        # ACT: Focus on something else.
+        print("")
+    # If it's gone
+    else:
+        PrintNested( "A cigarette tarantula lies buried in the ash, its legs sticking out in a silent prowl." )
+
+lr_seqtrash_undertable = "Check the destruction under the table."
+
+lr_seqtrash_goback = "Focus on something else than trash, finally."
+
+
+inp_livingroomhub = [ lr_seqceiling, lr_seqtrash ]
+
+def goto_LivingRoom_trash():
+    hub_livingroom = False
+    texthub = "You feel somewhat placid in the clutter, though the serenity hums a latent comedown."
+    hub_livingroom_trash = True
+    while hub_livingroom_trash:
+        treeinput = doinput()
+
+def goto_LivingRoom():
+    texthub = "Living room placeholder. The cats forgot to push the file..."
     hub_livingroom = True
     while hub_livingroom:
-        treeinput = doinput()
-        # PrintNested( "Man I'm tired" )
+        treeinput = doinput(texthub, inp_livingroomhub )
+        match treeinput:
+            case 1: PrintNested( lr_seqceiling_r )
+            case 2: 
+                PrintNested( lr_seqtrash_r )
+                goto_LivingRoom_trash()
 
 
-
-
-# Electrochemistry # 
-
-# Ars feline
-# Encyclopedia
-# Debugging
+# cereal bowl ashtray
+    # if you dig deeper with electrochem, you can find a half smoked joint, but you cant smoke it without a lighter
+    # you can smoke it or take it and give it to mef for special dialogue
+# balcony door
+# hallway
+# blanket on the couch
+# all the litter
+    # almost empty baggie. debugging and electrochem fight whether to slam or not
+    # if you do you can flirt with mef
+# pictures on the walls
+    # of mef. mostly her and her owner/her with friends at notable parties or events/a little bit of photography
+    # these give flags to talk to mef about if you give her the joint
+# mef kisses you if you:
+    # give her the joint
+    # mention her photography
+    # talk to her about her lighter from an ency check
+    # if you flirt with her from the baggie
 
 
 # One way to get to that street is by meeting [Blank], working on their car. You can talk to them by having Electrochem recognise the bumper sticker, or Debugging remembering what the issue might be.
 # "Shine pulled you out on an especially bright day to watch her replace the battery in her car."
 # If not that, then after walking around the city you can just bring up that he's been at this for a while.
+
+# FIX achievements??
+
+# START
+gotoDebugIntro()
